e0bc41f839c940a9c647ebe0a79f0bbeea36b627
Rafael Santos <rafa.05125@gmail.com>
2024-04-30 15:20:25 -0300

    Creating calculator functions

--- CalculatorTDD/Calculator.cs
+++ CalculatorTDD/Calculator.cs
@@ -2,5 +2,29 @@
 {
     public class Calculator
     {
+        public int add(int val1, int val2)
+        {
+            return 0;
+        }
+
+        public int subtract(int val1, int val2)
+        {
+            return 0;
+        }
+
+        public int multiply(int val1, int val2)
+        {
+            return 0;
+        }
+
+        public int divide(int val1, int val2)
+        {
+            return 0;
+        }
+
+        public List<string> historic()
+        {
+            return new List<string>();
+        }
     }
 }
