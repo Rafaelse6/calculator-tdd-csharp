60ccc43d82f5f1c5fce70a789a9d638324440c47
Rafael Santos <rafa.05125@gmail.com>
2024-04-30 17:09:59 -0300

    Historic implementation

--- CalculatorTDD/Calculator.cs
+++ CalculatorTDD/Calculator.cs
@@ -2,33 +2,55 @@
 {
     public class Calculator
     {
+
+        private List<string> historicList;
+
+        public Calculator()
+        {
+            historicList = new List<string>();
+        }
+
         public int add(int val1, int val2)
         {
             int res = val1 + val2;
+
+            historicList.Insert(0, "Res " + res);
+
             return res;
         }
 
         public int subtract(int val1, int val2)
         {
             int res = val1 - val2;
+
+            historicList.Insert(0, "Res " + res);
+
             return res;
         }
 
         public int multiply(int val1, int val2)
         {
             int res = val1 * val2;
+
+            historicList.Insert(0, "Res " + res);
+
             return res;
         }
 
         public int divide(int val1, int val2)
         {
             int res = val1 / val2;
+
+            historicList.Insert(0, "Res " + res);
+
             return res;
         }
 
         public List<string> historic()
         {
-            return new List<string>();
+
+            historicList.RemoveRange(3, historicList.Count - 3);
+            return historicList;
         }
     }
 }
